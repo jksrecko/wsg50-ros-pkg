5ff4540a2676ef73f1234dfa0b4d65fa8138d521
Denis Štogl <denisstogl@gmail.com>
2013-05-03 12:53:29 +0200

    Working version, after first debuging.

--- wsg_50_grasp_controller/scripts/grasp_controller.py
+++ wsg_50_grasp_controller/scripts/grasp_controller.py
@@ -6,7 +6,7 @@ import actionlib
 from wsg_50_common.srv import Move
 from wsg_50_common.msg import Cmd
 from sensor_msgs.msg import JointState
-from object_manipulation_msgs.action import GraspHandPostureExecution
+from object_manipulation_msgs.msg import GraspHandPostureExecutionAction, GraspHandPostureExecutionGoal
 from object_manipulation_msgs.srv import GraspStatus, GraspStatusResponse
 
 class WSG50GraspController:
@@ -19,25 +19,25 @@ class WSG50GraspController:
             try:
                 rospy.wait_for_service(gripper_service_grasp_name, 2)
                 rospy.loginfo('Found service on %s' % gripper_service_grasp_name)
-                gripper_grasp_srv = rospy.ServiceProxy(gripper_service_grasp_name, Move)
+                self.gripper_grasp_srv = rospy.ServiceProxy(gripper_service_grasp_name, Move)
                 break;
             except rospy.ROSException, e:
-                rospy.loginfo('Waiting for service on %s' gripper_service_grasp_name)
+                rospy.loginfo('Waiting for service on %s' % gripper_service_grasp_name)
 
         gripper_service_move_name = rospy.resolve_name('gripper_service_move_name')
         while(True):
             try:
                 rospy.wait_for_service(gripper_service_move_name, 2)
                 rospy.loginfo('Found service on %s' % gripper_service_move_name)
-                gripper_move_srv = rospy.ServiceProxy(gripper_service_move_name, Move)
+                self.gripper_move_srv = rospy.ServiceProxy(gripper_service_move_name, Move)
                 break;
             except rospy.ROSException, e:
-                rospy.loginfo('Waiting for service on %s' gripper_service_move_name)
+                rospy.loginfo('Waiting for service on %s' % gripper_service_move_name)
 
 
         self.gripper_virtual_joint_name = rospy.get_param('~gripper_virtual_joint_name', 'undefined')
         if self.gripper_virtual_joint_name == 'undefined':
-            rospy.logerror('Fingers joint names not defined set parameters gripper_virtual_joint_name in the launch file')
+            rospy.logerr('Fingers joint names not defined set parameters gripper_virtual_joint_name in the launch file')
 
 
         self.gripper_closed_gap_value = rospy.get_param('~gripper_closed_gap_value', 10)
@@ -52,7 +52,7 @@ class WSG50GraspController:
 
         # start action server
         posture_action_name = rospy.resolve_name('posture_action_name')
-        self.action_server = actionlib.SimpleActionServer(posture_action_name, GraspHandPostureExecution, execute_cb=self.execute_cb)
+        self.action_server = actionlib.SimpleActionServer(posture_action_name, GraspHandPostureExecutionAction, execute_cb=self.execute_cb)
         self.action_server.start()
         rospy.loginfo('WSG50 grasp hand posture action server started on topic %s' % posture_action_name)
 
@@ -64,45 +64,45 @@ class WSG50GraspController:
         ### Driver simulator do not implement speed regulation
         gripper_command.speed = 1
 
-        if goal.goal == GraspHandPostureExecution.GRASP:
+        if goal.goal == GraspHandPostureExecutionGoal.GRASP:
 
             if not goal.grasp.grasp_posture.position:
-                rospy.logerror("WSG grasp controller: position vector empty in requested grasp")
+                rospy.logerr("WSG grasp controller: position vector empty in requested grasp")
                 self.action_server.set_aborted()
                 return
 
-            gripper_command.mode = GraspHandPostureExecution.GRASP
+            gripper_command.mode = GraspHandPostureExecutionGoal.GRASP
 
-        elif goal.goal == GraspHandPostureExecution.PRE_GRASP:
+        elif goal.goal == GraspHandPostureExecutionGoal.PRE_GRASP:
 
             if not goal.grasp.pre_grasp_posture.position:
-                rospy.logerror("WSG grasp controller: position vector empty in requested pre_grasp")
+                rospy.logerr("WSG grasp controller: position vector empty in requested pre_grasp")
                 self.action_server.set_aborted()
                 return
 
-            gripper_command.mode = GraspHandPostureExecution.PRE_GRASP
-            gripper_command.pos = goal.grasp.pre_grasp_posture.position[0] * 2
+            gripper_command.mode = GraspHandPostureExecutionGoal.PRE_GRASP
+            gripper_command.pos = goal.grasp.pre_grasp_posture.position[1] * 2 * 1000 # in mm
 
-        elif goal.goal = GraspHandPostureExecution.RELEASE:
-            gripper_command.mode = GraspHandPostureExecution.RELEASE
+        elif goal.goal == GraspHandPostureExecutionGoal.RELEASE:
+            gripper_command.mode = GraspHandPostureExecutionGoal.RELEASE
             gripper_command.pos = self.gripper_opened_gap_value
             #gripper_command.max_effort = self.gripper_max_effort
 
         else:
-            rospy.logerror("WSG grasp controller: unknown goal code (%d)" % goal.goal)
-            action_server.set_aborted()
+            rospy.logerr("WSG grasp controller: unknown goal code (%d)" % goal.goal)
+            self.action_server.set_aborted()
             return
 
-        if gripper_command.mode == GraspHandPostureExecution.GRASP:
-            response = gripper_grasp_srv(gripper_command.pos, gripper_command.speed)
+        if gripper_command.mode == GraspHandPostureExecutionGoal.GRASP:
+            response = self.gripper_grasp_srv(gripper_command.pos, gripper_command.speed)
         else:
-            response = gipper_move_srv()
+            response = self.gripper_move_srv(gripper_command.pos, gripper_command.speed)
 
         if response == 0:
-            action_server.set_succeeded()
+            self.action_server.set_succeeded()
         else:
             rospy.logwarn("WSG grasp controller: gripper goal not achieved for pre-grasp or release")
-            action_server.set_succeeded()
+            self.action_server.set_succeeded()
 
 
     def service_callback(self, req):
@@ -127,7 +127,7 @@ class WSG50GraspController:
         joint_states = rospy.wait_for_message('joint_states', JointState)
 
         if not joint_states:
-            rospy.logerror("WSG grasp controller: joint states not received")
+            rospy.logerr("WSG grasp controller: joint states not received")
             return 0
 
         return joint_states.position[joint_states.name.index(self.left_finger_joint_name)]
